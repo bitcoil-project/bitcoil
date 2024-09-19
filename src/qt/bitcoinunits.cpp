51aae47bbc2f344c45f3182c26ec2e15457dec6b
Bitcoil <xcitytradingacademy@gmail.com>
2024-09-19 17:31:23 +0100

    Bitcoil branding

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -42,10 +42,10 @@ QString BitcoinUnits::longName(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("LTC");
-    case mBTC: return QString("lites");
+    case BTC: return QString("BCL");
+    case mBTC: return QString("bitcs");
     case uBTC: return QString("photons");
-    case SAT: return QString("liteoshi");
+    case SAT: return QString("bitcoshi");
     default: return QString("???");
     }
 }
@@ -64,10 +64,10 @@ QString BitcoinUnits::description(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("Litecoins");
-    case mBTC: return QString("Lites (1 / 1" THIN_SP_UTF8 "000)");
+    case BTC: return QString("Bitcoils");
+    case mBTC: return QString("Bitcs (1 / 1" THIN_SP_UTF8 "000)");
     case uBTC: return QString("Photons (1 / 1" THIN_SP_UTF8 "000" THIN_SP_UTF8 "000)");
-    case SAT: return QString("Liteoshis (sat) (1 / 100" THIN_SP_UTF8 "000" THIN_SP_UTF8 "000)");
+    case SAT: return QString("Bitcoshis (sat) (1 / 100" THIN_SP_UTF8 "000" THIN_SP_UTF8 "000)");
     default: return QString("???");
     }
 }
