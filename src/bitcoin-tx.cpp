c3be16e197e9a48fdc00783438f7e81e414800bb
Bitcoil <xcitytradingacademy@gmail.com>
2024-09-24 23:12:35 +0100

    Bitcoil branding

--- src/bitcoin-tx.cpp
+++ src/bitcoin-tx.cpp
@@ -98,8 +98,8 @@ static int AppInitRawTx(int argc, char* argv[])
     if (argc < 2 || HelpRequested(gArgs)) {
         // First part of help message is specific to this utility
         std::string strUsage = PACKAGE_NAME " litecoin-tx utility version " + FormatFullVersion() + "\n\n" +
-            "Usage:  litecoin-tx [options] <hex-tx> [commands]  Update hex-encoded litecoin transaction\n" +
-            "or:     litecoin-tx [options] -create [commands]   Create hex-encoded litecoin transaction\n" +
+            "Usage:  bitcoil-tx [options] <hex-tx> [commands]  Update hex-encoded bitcoil transaction\n" +
+            "or:     bitcoil-tx [options] -create [commands]   Create hex-encoded bitcoil transaction\n" +
             "\n";
         strUsage += gArgs.GetHelpMessage();
 
